3f9f13c077fe28ae00902c074d11fee341431291
lemonlemon544 <mihailbasov544@mail.ru>
2024-06-19 22:27:01 +0700

    Добавил товары

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(requests):
-    return HttpResponse("Hello, this is an internet appliation!")
+    return render(requests, 'index.html')
+
