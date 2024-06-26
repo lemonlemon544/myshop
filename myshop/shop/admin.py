75add456a67ad44312182fd121434856380ab692
lemonlemon544 <mihailbasov544@mail.ru>
2024-06-26 22:42:24 +0700

    Добавил модуль и админку

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
+
+class Product_Admin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
+admin.site.register(Product, Product_Admin)
