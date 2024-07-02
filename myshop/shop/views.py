e8707420074a04295fb393c5dfbe4da53f057dee
lemonlemon544 <mihailbasov544@mail.ru>
2024-07-02 23:27:57 +0700

    какие-то проблемы

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
+
 from .models import Product
 
 # Create your views here.
-def home(requests):
+
+def home(request):
     products = Product.objects.all()
 
-    return render(requests, "index.html", {
+    return render(request, "index.html", {
         'products': products
     })
 
-def view_product(request):
-    return render(request, 'product.html')
+def view_product(request, id):
+    product = Product.objects.filter(id=id).first()
+    return render(request, 'product.html', {
+        'product': product
+    })
