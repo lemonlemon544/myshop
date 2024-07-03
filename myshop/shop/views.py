2719eca5909caf32811aa269739d25339ec0749e
lemonlemon544 <mihailbasov544@mail.ru>
2024-07-03 22:50:31 +0700

    страница 404 все время

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -6,12 +6,19 @@ from .models import Product
 # Create your views here.
 
 def home(request):
-    print(request.GET)
+    search = request.GET.get('search')
 
     products = Product.objects.all()
 
+    products = Product.objects.all()
+    if search:
+        products = Product.objects.filter(name__contains=search).all()
+    else:
+        products = Product.objects.all()
+
     return render(request, "index.html", {
-        'products': products
+        'products': products,
+        'search': search,
     })
 
 def view_product(request, id):
